35732628b32997ee02f7f7f86f09839fea05ee51
mac718 <mac718@gmail.com>
2017-12-13 20:01:20 -0500

    wrote create in ArticlesController, set up show in articles/views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,17 @@
 class ArticlesController < ApplicationController
   def new 
   end
+
+  def create
+    @article = Article.new(:title => params['title'], :body => params['body'])
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
